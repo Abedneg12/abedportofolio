497b306b7a30c6257ac0bb6431872265b2127495
Abedneg12 <yosua12.abednego@gmail.com>
2025-08-19 00:09:32 +0700

    feat changing background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Iridescence from "@/components/Iridescence";
+import FaultyTerminal from "@/components/Faultyterminal";
+import { ReactNode } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,20 +16,38 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
-        <Iridescence
-          color={[0.5, 0, 0.1]}    // Warna default (R=1,G=1,B=1) => putih
-          speed={1.0}          // Kecepatan animasi
-          amplitude={0.1}        // Besar efek "goyang" mouse
-          mouseReact={true}
-          />    
-        <Navbar/>
-        {children}
+      <body className={`${inter.className} relative min-h-screen`}>
+        {/* BACKGROUND (tidak memengaruhi flow) */}
+        <div className="fixed inset-0 -z-10 pointer-events-none">
+          <FaultyTerminal
+            scale={1.5}
+            gridMul={[2, 1]}
+            digitSize={1.2}
+            timeScale={1}
+            pause={false}
+            scanlineIntensity={1}
+            glitchAmount={1}
+            flickerAmount={1}
+            noiseAmp={1}
+            chromaticAberration={0}
+            dither={0}
+            curvature={0}
+            tint="#FF2900"
+            mouseReact={true}
+            mouseStrength={0.5}
+            pageLoadAnimation={false}
+            brightness={1}
+          />
+        </div>
+
+        {/* KONTEN */}
+        <div className="relative z-10">
+          <Navbar />
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
